05127049a4854e52aca2d685ea5db5f60996eacf
Taehyun Kim <pztnakro@hotmail.com>
2017-11-29 06:21:09 +1300

    Added WebBot account info
    WebBot AppID, Password

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "99984d2b-e5d4-4577-98a8-abee95808c86",
+    appPassword: "pjnhvcDVY59401[-{vOGLJ:"
 });
 
 // Listen for messages from users 
